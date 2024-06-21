1941bbab677b4fa3ee2e6370cf9e9aa636a6c8a6
kitakou0313 <kitakou0313@gmail.com>
2024-06-21 12:42:45 +0000

    add: define method

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
+
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+		};
+	}
+
 
 }
