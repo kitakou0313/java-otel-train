d1fe7927ffc5ff55e721546b359cc3ea7770906b
kitakou0313 <kitakou0313@gmail.com>
2024-06-28 21:16:14 +0900

    test:Change test case

--- src/test/java/com/example/demo/contoller/HelloControllerTest.java
+++ src/test/java/com/example/demo/contoller/HelloControllerTest.java
@@ -22,7 +22,7 @@ public class HelloControllerTest {
     @Test
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
+				.andExpect(status().is2xxSuccessful())
 				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
 }
