a4c0c2f9ab6c9f806eb674084436011f9b3d9596
kitakou0313 <kitakou0313@gmail.com>
2024-07-05 22:05:01 +0900

    add: init tracer

--- src/main/java/com/example/demo/Dice.java
+++ src/main/java/com/example/demo/Dice.java
@@ -4,13 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.OpenTelemetry;
+
 public class Dice {
     private int min;
     private int max;
+    private Tracer tracer;
 
-    public Dice(int min, int max){
+    public Dice(int min, int max, OpenTelemetry openTelemetry){
         this.min = min;
         this.max = max;
+
+        this.tracer = openTelemetry.getTracer(
+          Dice.class.getName(), "0.1.0"
+        );
+    }
+
+    public Dice(int min, int max){
+      this(min, max, OpenTelemetry.noop());
     }
 
     private int rollOnce(){
