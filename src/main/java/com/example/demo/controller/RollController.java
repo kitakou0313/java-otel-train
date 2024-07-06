e13736cd0414cc020ee8fa88d6cd3c69b05cbc6f
kitakou0313 <kitakou0313@gmail.com>
2024-07-06 18:01:20 +0900

    add: Instrument index method

--- src/main/java/com/example/demo/controller/RollController.java
+++ src/main/java/com/example/demo/controller/RollController.java
@@ -11,7 +11,9 @@ import org.springframework.web.server.ResponseStatusException;
 import com.example.demo.Dice;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,19 +35,28 @@ public class RollController {
         @RequestParam("player") Optional<String> player, 
         @RequestParam("rolls") Optional<Integer> rolls) {
         
-        if (!rolls.isPresent()){
-            throw new ResponseStatusException(
-                HttpStatus.BAD_REQUEST, "Missng param"
-            );
+        Span span = tracer.spanBuilder("rollTheDice").startSpan();
+        try (Scope scope = span.makeCurrent())
+        {
+            if (!rolls.isPresent()){
+                throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Missng param"
+                );
+            }
+    
+            List<Integer> result = new Dice(1, 6).rollTheDice(rolls.get());
+            
+            if (player.isPresent()) {
+                logger.info("{} is rolling the dice: {}", player.get(), result);
+              } else {
+                logger.info("Anonymous player is rolling the dice: {}", result);
+              }
+              return result;
+        } catch(Throwable t){
+            span.recordException(t);
+            throw t;
+        }finally{
+            span.end();
         }
-
-        List<Integer> result = new Dice(1, 6).rollTheDice(rolls.get());
-        
-        if (player.isPresent()) {
-            logger.info("{} is rolling the dice: {}", player.get(), result);
-          } else {
-            logger.info("Anonymous player is rolling the dice: {}", result);
-          }
-          return result;
     }
 }
