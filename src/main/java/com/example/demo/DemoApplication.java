1460d173672d8b097c02cb14ef28e1f15f80daa2
kitakou0313 <kitakou0313@gmail.com>
2024-07-15 11:27:52 +0900

    fix:fix method to init object OpenTelemetry class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,38 +1,61 @@
 package com.example.demo;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.exporter.logging.LoggingMetricExporter;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.semconv.ResourceAttributes;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-
-			for (String beanName : beanNames){
-				System.out.println(beanName);
-			}
-		};
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+    	app.setBannerMode(Banner.Mode.OFF);
+    	app.run(args);
 	}
 
 	@Bean
-	public OpenTelemetry openTelemetry(){
-		return AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+  	public OpenTelemetry openTelemetry() {
+		Resource resource = Resource.getDefault().toBuilder().put(ResourceAttributes.SERVICE_NAME, "dice-server").put(ResourceAttributes.SERVICE_VERSION, "0.1.0").build();
+
+		SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+			.addSpanProcessor(SimpleSpanProcessor.create(LoggingSpanExporter.create()))
+			.setResource(resource)
+			.build();
+
+		SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
+			.registerMetricReader(PeriodicMetricReader.builder(LoggingMetricExporter.create()).build())
+			.setResource(resource)
+			.build();
+
+		SdkLoggerProvider sdkLoggerProvider = SdkLoggerProvider.builder()
+			.addLogRecordProcessor(BatchLogRecordProcessor.builder(SystemOutLogRecordExporter.create()).build())
+			.setResource(resource)
+			.build();
+
+    	return OpenTelemetrySdk.builder()
+			.setTracerProvider(sdkTracerProvider)
+			.setMeterProvider(sdkMeterProvider)
+			.setLoggerProvider(sdkLoggerProvider)
+			.setPropagators(ContextPropagators.create(TextMapPropagator.composite(W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance())))
+			.buildAndRegisterGlobal();
 	}
-
-
 }
--- src/main/java/com/example/demo/DiceApplication.java
+++ src/main/java/com/example/demo/DiceApplication.java
@@ -1,42 +1,43 @@
-package com.example.demo;
+// package com.example.demo;
 
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
+// import org.springframework.boot.Banner;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.context.annotation.Bean;
 
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
-import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.context.propagation.TextMapPropagator;
-import io.opentelemetry.exporter.logging.LoggingSpanExporter;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
-import io.opentelemetry.semconv.ResourceAttributes;
+// import io.opentelemetry.api.OpenTelemetry;
+// import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
+// import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+// import io.opentelemetry.context.propagation.ContextPropagators;
+// import io.opentelemetry.context.propagation.TextMapPropagator;
+// import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+// import io.opentelemetry.sdk.OpenTelemetrySdk;
+// import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+// import io.opentelemetry.sdk.resources.Resource;
+// import io.opentelemetry.sdk.trace.SdkTracerProvider;
+// import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+// import io.opentelemetry.semconv.ResourceAttributes;
 
-public class DiceApplication {
- public static void main(String[] args) {
+// public class DiceApplication {
+//  public static void main(String[] args) {
     
-    SpringApplication app = new SpringApplication(
-        DiceApplication.class
-    );
-    app.setBannerMode(Banner.Mode.OFF);
-    app.run(args);
- }
- 	@Bean
-	public OpenTelemetry openTelemetry(){
-        Resource resource = Resource.getDefault().toBuilder().put(ResourceAttributes.SERVICE_NAME, "dice-server").put(ResourceAttributes.SERVICE_VERSION, "0.1.0").build();
+//     SpringApplication app = new SpringApplication(
+//         DiceApplication.class
+//     );
+//     app.setBannerMode(Banner.Mode.OFF);
+//     app.run(args);
+//  }
 
-        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-            .addSpanProcessor(SimpleSpanProcessor.create(LoggingSpanExporter.create()))
-            .setResource(resource).build();
+// @Bean
+// public OpenTelemetry openTelemetry(){
+//     Resource resource = Resource.getDefault().toBuilder().put(ResourceAttributes.SERVICE_NAME, "dice-server").put(ResourceAttributes.SERVICE_VERSION, "0.1.0").build();
 
-        return OpenTelemetrySdk.builder()
-            .setTracerProvider(sdkTracerProvider)
-            .setPropagators(ContextPropagators.create(TextMapPropagator.composite(W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance())))
-            .buildAndRegisterGlobal();
-    }
-}
+//     SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+//         .addSpanProcessor(SimpleSpanProcessor.create(LoggingSpanExporter.create()))
+//         .setResource(resource).build();
+    
+//     return OpenTelemetrySdk.builder()
+//         .setTracerProvider(sdkTracerProvider)
+//         .setPropagators(ContextPropagators.create(TextMapPropagator.composite(W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance())))
+//         .buildAndRegisterGlobal();
+// }
+// }
